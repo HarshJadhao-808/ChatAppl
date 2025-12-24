adf83142a07f3efad97e41e9671d807e040787c2
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-12-24 17:31:18 +0530

    connected with frontend

--- Back-End/src/index.js
+++ Back-End/src/index.js
@@ -1,25 +1,67 @@
-import express from 'express'
-import cors from 'cors'
-import http from 'http'
+import express from "express";
+import cors from "cors";
+import http from "http";
 import { Server } from "socket.io";
 
-const app = express()
-const server = http.createServer(app) 
-app.use(express.json())
-app.use(cors())
-const PORT = 8000
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = 8000;
 
 app.get("/", (req, res) => {
-	res.send("Socket.IO chat server running");
+  res.send("Socket.IO chat server running");
 });
 
 
-server.listen(PORT,()=>{
-    console.log(`chat server is running on http://localhost:${PORT}`)
-})
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+
+  socket.on("joined", ({ user }) => {
+    socket.username = user;
+
+    socket.emit("welcome", {
+      user: "Server",
+      message: `Welcome ${user}`,
+    });
+
+    socket.broadcast.emit("userJoined", {
+      user: "Server",
+      message: `${user} joined the chat`,
+    });
+  });
+
+
+  socket.on("message", ({ message }) => {
+    io.emit("sendMessage", {
+      user: socket.username,
+      message,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      socket.broadcast.emit("leave", {
+        user: "Server",
+        message: `${socket.username} left the chat`,
+      });
+    }
+  });
+});
 
 
 
-
-
-
+server.listen(PORT, () => {
+  console.log(`Chat server running on http://localhost:${PORT}`);
+});
